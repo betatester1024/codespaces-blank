f9533782193d9a36213084e0edbf1be5ebb2c460
Jennifer Green <betaos.services@gmail.com>
2025-05-24 15:42:17 +0000

    perhaps now it'll have python (serverless function moment)

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,18 @@ const nextConfig: NextConfig = {
     // config.optimization.minimizer = [];
     return config
   },
-  turbopack: {}
+  turbopack: {},
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
   /* config options here */
 };
 export default nextConfig;
